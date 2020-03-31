8c781fc133b83dcbe836c7ddac18546600884499
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-03-31 21:26:50 +0530

    Thanks to @RaZeR877

--- hosts.sh
+++ hosts.sh
@@ -12,8 +12,7 @@ git clone https://github.com/Ayush1311/PROJECT-ANONYMOUS/hosts.sh
 
 #--FUNCTION--#
 > /system/etc/hosts
-printf ".net\n.org\n.in\n.mobi\n.wiki\n.us\n.qq.com\n.helpshift.com\n.co\n.igamecj.com\n.biz\n.tel\n.tencent.com\n.qcloud.com\n.dns.com\n.proximabeta.com\n.cnn.com\n.cnzz.com\n.igamecj.com\n.tencent.com\n.pubgmobile.com\n.gcloud.com\n.gcloudcs.com" > /system/etc/hosts
-
+printf ".net\n.org\n.in\n.mobi\n.wiki\n.us\n.qq.com\n.helpshift.com\n.co\n.igamecj.com\n.biz\n.tel\n.tencent.com\n.qcloud.com\n.dns.com\n.proximabeta.com\n.cnn.com\n.cnzz.com\n.igamecj.com\n.tencent.com\n.pubgmobile.com\n.gcloud.com\n.gcloudcs.com\n.myqcloud.com\n.imsdk.com\n.amazonaws.com\n.1e100.net" > /system/etc/hosts
 #--WARNING = .com domains should not be blocked--#
 
 #--CREDITS--#
