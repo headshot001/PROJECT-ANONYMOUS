8b561bd156005c181c3e6f485c7d6244bac13511
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-03-31 21:23:00 +0530

    Update hosts.sh

--- hosts.sh
+++ hosts.sh
@@ -8,10 +8,9 @@
 cd $HOME/PROJECT-ANONYMOUS
 rm hosts.sh
 git clone https://github.com/Ayush1311/PROJECT-ANONYMOUS/hosts.sh
-#------------------ --#
+#---------------------#
 
 #--FUNCTION--#
-chmod 664 /system/etc/hosts
 > /system/etc/hosts
 printf ".net\n.org\n.in\n.mobi\n.wiki\n.us\n.qq.com\n.helpshift.com\n.co\n.igamecj.com\n.biz\n.tel\n.tencent.com\n.qcloud.com\n.dns.com\n.proximabeta.com\n.cnn.com\n.cnzz.com\n.igamecj.com\n.tencent.com\n.pubgmobile.com\n.gcloud.com\n.gcloudcs.com" > /system/etc/hosts
 
