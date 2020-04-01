2afb73e823ca6de9eb81470ea46f3e498e222723
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-04-01 16:31:20 +0530

    Update hosts.sh

--- hosts.sh
+++ hosts.sh
@@ -13,6 +13,7 @@ git clone https://github.com/Ayush1311/PROJECT-ANONYMOUS/hosts.sh
 #--FUNCTION--#
 > /system/etc/hosts
 printf ".com\n.net\n.org\n.in\n.mobi\n.wiki\n.us\n.qq.com\n.helpshift.com\n.co\n.igamecj.com\n.biz\n.tel\n.tencent.com\n.qcloud.com\n.dns.com\n.proximabeta.com\n.cnn.com\n.cnzz.com\n.igamecj.com\n.tencent.com\n.pubgmobile.com\n.gcloud.com\n.gcloudcs.com\n.myqcloud.com\n.imsdk.com\n.amazonaws.com\n.1e100.net" > /system/etc/hosts
+
 #--CREDITS--#
 #@AyushChatterjee(t.me/AyushChatterjee)
 #@itsMercy(t.me/itsmercy)
