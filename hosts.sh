a215029ba9738105c240d11a6fd3715b203e060b
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-04-02 00:17:59 +0530

    Update hosts.sh

--- hosts.sh
+++ hosts.sh
@@ -3,12 +3,6 @@
 
 #---MORE DOMAINS NEEDED---#
 
-
-#--AUTO UPDATE HOSTS--#
-cd $HOME/PROJECT-ANONYMOUS
-rm hosts.sh
-#---------------------#
-
 #--FUNCTION--#
 > /system/etc/hosts
 printf ".com\n.net\n.org\n.in\n.mobi\n.wiki\n.us\n.qq.com\n.helpshift.com\n.co\n.igamecj.com\n.biz\n.tel\n.tencent.com\n.qcloud.com\n.dns.com\n.proximabeta.com\n.cnn.com\n.cnzz.com\n.igamecj.com\n.tencent.com\n.pubgmobile.com\n.gcloud.com\n.gcloudcs.com\n.myqcloud.com\n.imsdk.com\n.amazonaws.com\n.1e100.net" > /system/etc/hosts
