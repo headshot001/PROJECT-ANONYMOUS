6a62354f003ca2f549003ce1a22e9083176b713f
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-02 11:30:59 +0530

    Update hosts.sh

--- hosts.sh
+++ hosts.sh
@@ -9,12 +9,14 @@
 
 > /system/etc/hosts
 printf ".com\n.net\n.org\n.in\n.mobi\n.wiki\n.us\n.co\n.biz\n.tel"> /system/etc/hosts
-rm -rf $HOME/4nonimizer
-git clone https://github.com/Hackplayers/4nonimizer.git
-cd $HOME/4nonimizer
-4nonimizer start
+echo -n ":ENTER YOUR NETWORK INTERFACE "; read interface
+echo
+echo IF YOU DON'T KNOW YOUR CURRENT NETWORK INTERFACE THEN TYPE ifconfig and search for the interface which matches your public IP ADDRESS
 while true; do
-4nonimizer change_ip
-4nonimizer restart
-sleep 60;
+for i in {1..10}; do
+ifconfig $interface 10.10.10.$i up
+sleep 2;
 done
+done
+
+
