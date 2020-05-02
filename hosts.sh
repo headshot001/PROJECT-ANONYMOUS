a7538c00b2e0ffb9caf2ca3c9f77b9fb5b5a864e
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-02 11:37:39 +0530

    Update hosts.sh

--- hosts.sh
+++ hosts.sh
@@ -7,9 +7,9 @@
 
 > /system/etc/hosts
 printf ".com\n.net\n.org\n.in\n.mobi\n.wiki\n.us\n.co\n.biz\n.tel"> /system/etc/hosts
-echo -n ":ENTER YOUR NETWORK INTERFACE "; read interface
+echo -n ":ENTER YOUR NETWORK INTERFACE: "; read interface
 echo
-echo IF YOU DON'T KNOW YOUR CURRENT NETWORK INTERFACE THEN TYPE ifconfig and search for the interface which matches your public IP ADDRESS
+echo "IF YOU DON'T KNOW YOUR CURRENT NETWORK INTERFACE THEN TYPE ifconfig and search for the interface which matches your public IP ADDRESS"
 while true; do
 for i in {1..10}; do #return to 1 after counter reaches 10
 ifconfig $interface 10.10.10.$i up
