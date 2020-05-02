7eb95b274e923fa148b3044206fb63cb7bbef8ea
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-02 11:35:07 +0530

    Update hosts.sh

--- hosts.sh
+++ hosts.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #PROJECT-ANONYMOUS COPYRIGHT MIT LICENCE (c) 2020 Ayush Chatterjee
 
-#---MORE DOMAINS NEEDED---#
-
 #--FUNCTION--#
-#--USES 4nonimizer for auto-ip-change--#
-#--PLEASE READ THE GUIDE HERE https://github.com/Hackplayers/4nonimizer.git--#
+#--auto-ip-change--#
+
 
 > /system/etc/hosts
 printf ".com\n.net\n.org\n.in\n.mobi\n.wiki\n.us\n.co\n.biz\n.tel"> /system/etc/hosts
@@ -13,9 +11,9 @@ echo -n ":ENTER YOUR NETWORK INTERFACE "; read interface
 echo
 echo IF YOU DON'T KNOW YOUR CURRENT NETWORK INTERFACE THEN TYPE ifconfig and search for the interface which matches your public IP ADDRESS
 while true; do
-for i in {1..10}; do
+for i in {1..10}; do #return to 1 after counter reaches 10
 ifconfig $interface 10.10.10.$i up
-sleep 2;
+sleep 60; #minimum time interval don't change it 
 done
 done
 
